64b6ffff79eae8240cd50738a1d8df63bcf1538d
Eraliev006 <eraliev.adilet006@gmail.com>
2024-10-31 12:30:08 +0600

    update: updated create and get_news method

--- news_service/service/news_service.py
+++ news_service/service/news_service.py
@@ -1,5 +1,7 @@
+
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy import select
 
 from fastapi import HTTPException
 
@@ -19,7 +21,16 @@ class NewsService:
             await self._db.commit()
             await self._db.refresh(new_news)
 
-            return NewsOut.from_orm(news)
+            return NewsOut.from_attributes(news)
         except SQLAlchemyError as e:
             await self._db.rollback()
             raise HTTPException(status_code=500, detail=f'Error while creating new news - {e}')
+
+    async def get_news(self) -> list[NewsOut]:
+        try:
+            result = await self._db.execute(select(News))
+            news_list = result.scalars().all()
+
+            return [NewsOut.from_attributes(x) for x in news_list]
+        except SQLAlchemyError as e:
+            raise HTTPException(status_code=500, detail=f'Error while getting news - {e}')
